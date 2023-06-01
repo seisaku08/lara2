f9ecaaaca3e071c5d98e55c9514f49c9a682959f
seisaku08 <seisaku08@dai-oh.co.jp>
2023-06-01 18:54:18 +0900

    ああ

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('test:info')->everyMinute();
+        // $schedule->command('test:info')->everyMinute();
         // $schedule->command('inspire')->hourly();
     }
 
