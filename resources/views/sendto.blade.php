c60403601a1ca31554c1a40a78c39e5ead41a0fa
seisaku08 <seisaku08@dai-oh.co.jp>
2023-06-14 13:57:54 +0900

    test

--- resources/views/sendto.blade.php
+++ resources/views/sendto.blade.php
@@ -53,7 +53,7 @@
                 <th>配送先情報</th>
             </tr>
             <tr>
-                <td class="w100">配送先情報を後日入力する場合は、チェックボックスにチェックを入れてください。→<input type="checkbox" name="seminar_venue_pending" placeholder="" value="true"{{ old('seminar_venue_pending') == true ? ' checked' : '' }}></td>
+                    <td class="w100">配送先情報を後日入力する場合は、チェックボックスにチェックを入れてください。→<input type="checkbox" name="seminar_venue_pending" placeholder="" value="true"{{ old('seminar_venue_pending') == true ? ' checked' : '' }}></td>
             </tr>
             <tr>
                 <td class="w30"><label>郵便番号</label><span class="red small">＊必須</span></td>
