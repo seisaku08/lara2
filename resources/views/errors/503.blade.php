57d986c485a62e3b206e9448103c739303833ca6
seisaku08 <seisaku08@dai-oh.co.jp>
2024-03-13 16:01:35 +0900

    aa

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,15 @@
-@extends('errors::minimal')
+{{-- @extends('errors::minimal')
 
 @section('title', __('Service Unavailable'))
 @section('code', '503')
-@section('message', __('Service Unavailable'))
+@section('message', __('Service Unavailable')) --}}
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>メンテナンス中</title>
+    </head>
+    <body>
+        <h1>只今メンテナンス中です。</h1>
+    </body>
+</html>
