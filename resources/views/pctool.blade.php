b66921d0689ce12061e2ff1c1ffea921cc1cfd58
seisaku08 <seisaku08@dai-oh.co.jp>
2024-03-29 20:54:41 +0900

    aa

--- resources/views/pctool.blade.php
+++ resources/views/pctool.blade.php
@@ -18,7 +18,7 @@
         「予約開始日」は<b>セミナー開催日の4営業日以前（上記の場合、{{ App\Libs\Common::dayafter(today(),1)->isoFormat('YYYY年M月D日（ddd）'); }}）</b><br>
         「予約終了日」は<b>セミナー開催日（複数日開催、または連続使用の場合はその最終日）の3営業日以降（上記の場合、{{ App\Libs\Common::dayafter(today(),8)->isoFormat('YYYY年M月D日（ddd）'); }}）</b>
       </p>
-      </div>
+      </div> 
     </div>　
     <b> ＜参考＞</b>荷物の配送所要日数は<a href="http://date.kuronekoyamato.co.jp/date/Main?LINK=TK" target="_blank"><b>こちら</b></a>から検索できます（ヤマト運輸のサイトが開きます）
 
